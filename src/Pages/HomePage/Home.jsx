ce4898d5668dbb7ae94d0ed455bc6d673654a0c7
MD Tariqul Islam <ittarek551@gmail.com>
2023-05-18 17:25:33 +0600

    react tabs created

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -3,12 +3,14 @@ import NavigationBar from "../../Shared-componets/Header/NavigationBar";
 import Banner from "./BannarPage/Banner";
 import Footer from "../../Shared-componets/Footer/Footer";
 import Gallery from "../GallarySection/Gallery";
+import ShopCategory from "../ShopCategoryPage/ShopCategory";
 
 const Home = () => {
   return (
     <>
       <Banner></Banner>
       <Gallery></Gallery>
+      <ShopCategory></ShopCategory>
     </>
   );
 };
