7267336705c08b145e2a53964453102425e53901
MD Tariqul Islam <ittarek551@gmail.com>
2023-05-21 23:41:51 +0600

    bannar text italic

--- src/Pages/HomePage/BannarPage/Banner.jsx
+++ src/Pages/HomePage/BannarPage/Banner.jsx
@@ -6,8 +6,8 @@ const Banner = () => {
     <Container className="d-lg-flex align-items-center" data-aos="zoom-in-down">
       <div className="heading">
         <h1 className="text-secondary fs-2"> <span className="text-info fs-1">Tech Toys</span> Galore: Unleash Your Inner Geek and Explore the Latest Gadgets! </h1>
-        <p >
-        Welcome to <span className="text-info fs-3 fw-bold">Tech Toys</span> Galore, the ultimate destination for all tech enthusiasts and geeks! We're here to bring you an exciting world of cutting-edge gadgets and innovative toys that will ignite your imagination and keep you entertained for hours on end.
+        <p className=""><em>
+        Welcome to <span className="text-info fs-3 fw-bold">Tech Toys</span> Galore, the ultimate destination for all tech enthusiasts and geeks! We're here to bring you an exciting world of cutting-edge gadgets and innovative toys that will ignite your imagination and keep you entertained for hours on end.</em>
         </p>
       </div>
 
